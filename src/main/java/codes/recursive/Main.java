d505a2dec0b18b4f70e9951dd5b357de51e53747
Inger Rova <inger@Ingers-iMac.local>
2022-03-12 21:17:36 +0100

    first commit

---
+++ src/main/java/codes/recursive/Main.java
@@ -0,0 +1,34 @@
+package codes.recursive;
+
+import com.sun.net.httpserver.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        int port = args.length > 0 ? Integer.parseInt(args[0]) : Integer.parseInt( System.getProperty("port", "8080") );
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+
+        logger.info("Listening on localhost:{}...", port);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Server shutting down. Goodbye...");
+            try {
+                server.stop(1000);
+            }
+            catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        }));
+
+    }
+
+}
---
+++ src/main/java/codes/recursive/MyHandler.java
@@ -0,0 +1,32 @@
+package codes.recursive;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+class MyHandler implements HttpHandler {
+    Logger logger = LoggerFactory.getLogger(Main.class);
+
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+        logger.info("Got request {} {} {} {}", t.getRequestMethod(), t.getRequestURI().getPath(), dumpHeaders(t), t.getRemoteAddress());
+
+        String response = "Thanks";
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+
+    private String dumpHeaders(HttpExchange t) {
+        return t.getRequestHeaders().entrySet().stream().map((Function<Map.Entry<String, List<String>>, Object>) e -> String.format("%s: %s", e.getKey(), e.getValue().get(0))).collect(Collectors.toList()).toString();
+    }
+}
